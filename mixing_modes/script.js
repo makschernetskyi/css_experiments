ef488817c74a00db6ebc0a969d4fe94fa0989e29
Maksym Chernetskyi <konomerubokkusu@gmail.com>
2023-05-08 18:14:26 +0200

    init

---
+++ mixing_modes/script.js
@@ -0,0 +1,10 @@
+const green = document.getElementsByClassName('green')[0];
+const container = document.getElementsByClassName('container')[0];
+
+
+// container.onmousemove = e =>{
+// 	let x = e.pageX;
+// 	let y = e.pageY;
+// 	green.style.top = y-50;
+// 	green.style.left = x-50;
+// }
